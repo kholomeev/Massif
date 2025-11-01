315ebf2ba907a9251aaf1224ae2833e61c401889
kholomeev <holomeev2011@gmail.com>
2025-11-01 13:03:03 +0300

    Задание 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,25 +13,51 @@
 fun main() {
     print("Введите желаемую вами размерность двумерного массива: ")
     val arrSize = readln().trim()   // Записываем размерность матрицы
+    if (arrSize.isEmpty()) {
+        println("Размерность не указана.")
+        return
+    }
 
     // Разделяем входные данные на две переменные
     val arrSizeCl = arrSize.split(" ")
-    val col = arrSizeCl[0].toInt()
-    val row = arrSizeCl[1].toInt()
+    if (arrSizeCl.size != 2) {
+        println("Не все параметры размерности были указаны, или их больше, чем два.")
+        return
+    }
+    val row = arrSizeCl[0].toInt()
+    val col = arrSizeCl[1].toInt()
 
     // Генерация двумерного массива
-    println("Двумерный массив:")
-    val arrTwoDimens: Array<Array<Int>> = Array(col) {
-        Array(row) {
-            (100..999).random() // Ввод элементов
+    val matrix = Array(row) { IntArray(col) }
+    for (i in 0 until row) {
+        val rowInput = readln().split(" ").map { it.toInt() }
+        // Проверяем, совпадает ли количество введенных элементов с количеством столбцов
+        if (rowInput.size == col) {
+            for (j in 0 until col) {
+                matrix[i][j] = rowInput[j]
+            }
+        }
+        else {
+            println("Ошибка: количество элементов в строке не совпадает с количеством столбцов ($col). Попробуйте снова.")
+            return
         }
     }
+
     // Количество различных цифр в матрице
+    val diffDigits = mutableSetOf<String>()
+    for (row in matrix) {
+        for (element in row) {
+            val digits = arrSize.split("")
+        }
+    }
+
+    println(diffDigits)
 
     // Вывод на консоль
+    println("Двумерный массив:")
     for (i in 0 until col) {
         for (j in 0 until row) {
-            print(arrTwoDimens[i][j].toString() + "\t\t")
+            print(matrix[i][j].toString() + "\t\t")
         }
         println()
     }
