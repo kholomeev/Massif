733eee33bab14030aeefbb5cb1cdc894a6847570
kholomeev <holomeev2011@gmail.com>
2025-11-22 14:44:00 +0300

    Задания 1, 2, 3, (4), 5

--- src/main/kotlin/Task5.kt
+++ src/main/kotlin/Task5.kt
@@ -1,18 +1,8 @@
 fun main() {
-    /* Создать приложение, в котором пользователь вводит массив из различных слов.
-       На выходе приложение должно показать слова сгруппированные по признаку "состоят из одинаковых букв".
-       Например, на входе ["eat", "tea", "tan", "ate", "nat", "bat"].
-
-       Получаем группы:
-       "ate", "eat", "tea"
-       "nat", "tan"
-       "bat"           */
-
-    // Ввод массива
     print("Введите несколько слов на английском: ")
     var words: List<String> = readln().split(" ")
     words = words.sorted()
-    // Деление на группы и считывание слов
+
     val groups = groupByUniqueLetters(words)
     groups.forEach { (key, value) ->
         println("$value")
