b732e6d04e2e60075682400a56de99e1fd9abf08
kholomeev <holomeev2011@gmail.com>
2025-11-29 12:15:06 +0300

    Выполнено

--- src/main/kotlin/Task4.kt
+++ src/main/kotlin/Task4.kt
@@ -1,34 +1,12 @@
 fun main() {
-    // Создать приложение, в котором пользователь дает на вход два числовых массива, числа в массиве могут повторяться.
-    // На выходе приложение выдает пересечение этих массивов.
-    // Например, на входе [1, 2, 3, 2, 0] и [5, 1, 2, 7, 3, 2, 2].
-    // На выходе должны получить [1, 2, 2, 3].
-    // То есть учитывается количество повторений чисел в массиве. Число 2 в одном массиве повторялось два раза, в другом - три.
-    // Значит в итоговом массиве число два должно быть два раза
-
-    // Ввод массивов
+    println("Пересечение значений двух массивов и перенос их в третий")
     println("Введите два числовых массива:")
-    var arr1: IntArray = readln().split(" ").map { it.toInt() }.toIntArray()
-    var arr2: IntArray = readln().split(" ").map { it.toInt() }.toIntArray()
-    // Образование пересечения массивов
-    arr1.sort() // 0 1 2 2 3
-    arr2.sort() // 1 2 2 2 3 5 7
-    // Формирование третьего массива
-    var arr1Map: MutableMap<Int, Int> = mutableMapOf()
-    for (num in arr1) {
-        val count = arr1Map.getOrDefault(num, 0)
-        arr1Map[num] = count + 1
-    }
-    var arr2Map: MutableMap<Int, Int> = mutableMapOf()
-    for (num in arr2) {
-        val count = arr2Map.getOrDefault(num, 0)
-        arr2Map[num] = count + 1
-    }
-    //
-    var arr3 = arr1Map.keys intersect arr2Map.keys
-    print(arr3)
-    var arr4 = arr1Map.entries intersect arr2Map.entries
-    print(arr4)
-    // Вывод массива
 
+    val arr1: IntArray = readln().split(" ").map { it.toInt() }.toIntArray()
+    arr1.sort()
+    val arr2: IntArray = readln().split(" ").map { it.toInt() }.toIntArray()
+    arr2.sort()
+
+    val arr3: IntArray = arr1.filter { it in arr2 }.toIntArray()
+    println("Результат: ${arr3.joinToString()}")
 }
