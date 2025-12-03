1caa28a734a9c693896c0d7f400715ce6f8f730b
kirill <holomeeev2011@gmail.com>
2025-12-04 00:17:06 +0300

    Отладка 1 и 2 и 4

--- src/main/kotlin/Task4.kt
+++ src/main/kotlin/Task4.kt
@@ -1,11 +1,32 @@
 fun main() {
-    println("Пересечение значений двух массивов и перенос их в третий")
-    println("Введите два числовых массива:")
+    var arr1: IntArray
+    var arr2: IntArray
 
-    val arr1: IntArray = readln().split(" ").map { it.toInt() }.toIntArray()
-    arr1.sort()
-    val arr2: IntArray = readln().split(" ").map { it.toInt() }.toIntArray()
-    arr2.sort()
+    println("Пересечение значений двух массивов и перенос их в третий")
+
+    while (true) {
+        try {
+            print("Введите 1 числовой массив: ")
+            arr1 = readln().split(" ").map { it.toInt() }.toIntArray()
+            arr1.sort()
+            break
+        }
+        catch (e: NumberFormatException) {
+            println(e.message)
+        }
+    }
+
+    while (true) {
+        try {
+            print("Введите 2 числовой массив: ")
+            arr2 = readln().split(" ").map { it.toInt() }.toIntArray()
+            arr2.sort()
+            break
+        }
+        catch (e: NumberFormatException) {
+            println(e.message)
+        }
+    }
 
     val arr3: IntArray = arr1.filter { it in arr2 }.toIntArray()
     println("Результат: ${arr3.joinToString()}")
